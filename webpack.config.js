94fa230c8d0fc3fa4d8e4d321c23befff8d1f666
Sebastian Leu <sebastian.leu1@gmail.com>
2019-05-20 16:35:21 +0700

    Make the plugin compatible with IE11

--- webpack.config.js
+++ webpack.config.js
@@ -10,16 +10,24 @@
 const path = require( 'path' );
 const webpack = require( 'webpack' );
 const { bundler } = require( '@ckeditor/ckeditor5-dev-utils' );
-const UglifyJsWebpackPlugin = require( 'uglifyjs-webpack-plugin' );
+// const UglifyJsWebpackPlugin = require( 'uglifyjs-webpack-plugin' );
 
 module.exports = {
 	mode: 'production',
 	devtool: 'source-map',
-	entry: path.join( __dirname, 'src', 'plugin.js' ),
+	entry: [
+		require.resolve( '@babel/polyfill' ),
+		require.resolve( 'unorm' ),
+		require.resolve( './src/ie11-polyfills.js' ),
+		require.resolve( 'regenerator-runtime/runtime.js' ),
+		path.join( __dirname, 'src', 'plugin.js' ),
+		//path.resolve( __dirname, 'src', 'plugin.js' ),
+	],
+	//
+	// entry: path.join( __dirname, 'src', 'plugin.js' ),
 
 	output: {
 		library: 'CKEditor',
-
 		path: path.join( __dirname, 'dist' ),
 		filename: 'ckeditor.js',
 		libraryTarget: 'umd',
@@ -27,17 +35,7 @@ module.exports = {
 	},
 
 	optimization: {
-		minimizer: [
-			new UglifyJsWebpackPlugin( {
-				sourceMap: true,
-				uglifyOptions: {
-					output: {
-						// Preserve CKEditor 5 license comments.
-						comments: /^!/
-					}
-				}
-			} )
-		]
+		minimizer: []
 	},
 
 	plugins: [
@@ -53,7 +51,39 @@ module.exports = {
 				test: /\.js$/,
 				loader: 'babel-loader',
 				exclude: /node_modules/,
-			}
+			},
+			{
+				test: /ckeditor5-[^/\\]+[/\\].*\.js$/,
+				use: [
+					{
+						loader: 'babel-loader',
+						options: {
+							presets: [
+								[
+									'@babel/preset-env', {
+										debug: true
+									}
+								]
+							],
+							plugins: [
+								[
+									'@babel/plugin-transform-modules-commonjs', {
+										strictMode: false
+									}
+								]
+							]
+						}
+					}
+				]
+			},
+			{
+				test: /\.js$/,
+				use: [
+					{
+						loader: require.resolve( './src/remove-strict.js' )
+					}
+				]
+			},
 		]
 	},
 };
